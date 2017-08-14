5cb1f48c149155f6e8e2693bf0eb1866fd36e3ee
Paul Crane <paul@acheronsoft.com>
2017-08-14 01:30:47 -0500

    Remove code related to command line interface.
    This code was split off into keephdalive-cli.

--- source/keephdalive/writer.d
+++ source/keephdalive/writer.d
@@ -7,10 +7,63 @@ import simpletimers.repeating;
 
 class KeepAliveWriter : RepeatingTimer
 {
+	this()
+	{
+
+	}
+
 	void addLocation(const string path)
 	{
 		locations_ ~= path;
 	}
+
+		void loadWriteToLocations()
+	{
+		immutable string locationsFile = buildNormalizedPath(path_.getDir("config"), WRITE_TO_LOCATIONS_FILENAME);
+
+		ensureFileExists(locationsFile, DEFAULT_LOCATIONS_DATA);
+		immutable auto lines = locationsFile.readText.splitLines();
+
+		foreach(filePath; lines)
+		{
+			addPath(filePath);
+		}
+	}
+
+	bool addPath(const string path, const bool shouldWrite = false)
+	{
+		if(path.exists)
+		{
+			immutable string normalizedFilePath = buildNormalizedPath(path, writeToFileName_);
+			immutable string locationsFile = buildNormalizedPath(path_.getDir("config"), WRITE_TO_LOCATIONS_FILENAME);
+			immutable bool alreadyKnownLocation = locationAlreadyExists(path);
+
+			if(!alreadyKnownLocation)
+			{
+				if(shouldWrite)
+				{
+					auto f = File(locationsFile, "a");
+					f.writeln(path);
+				}
+
+				locations_ ~= path;
+				writer_.addLocation(normalizedFilePath);
+
+				writeln("Added new path: ", path);
+			}
+			else
+			{
+				writeln("That path already exists!");
+			}
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
 
 	override void onTimer()
 	{
@@ -26,4 +79,5 @@ class KeepAliveWriter : RepeatingTimer
 
 private:
 	string[] locations_;
+	ConfigPath path_;
 }
