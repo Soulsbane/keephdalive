e8a7649f79d9c7ca8616ac6981a5ab6bf4c0ad45
Paul Crane <paul@acheronsoft.com>
2018-08-09 20:56:15 -0500

    Initial code for reworking the package into a daemon.

--- source/keephdalive/app.d
+++ source/keephdalive/app.d
@@ -1,4 +1,47 @@
-void main(string[] arguments)
-{
+import core.time;
+import core.thread;
 
+import daemonize.d;
+
+enum SettingsUpdatedSignal = "SettingsUpdated".customSignal;
+
+alias daemon = Daemon!(
+	"keephdalive",
+
+	KeyValueList!(
+		Composition!(Signal.Terminate, Signal.Quit, Signal.Shutdown, Signal.Stop), (logger)
+		{
+			logger.logInfo("Exiting...");
+			return false;
+		},
+		Signal.HangUp, (logger)
+		{
+			logger.logInfo("Hello World!");
+			return true;
+		},
+		SettingsUpdatedSignal, (logger)
+		{
+			logger.logInfo("Updated settings...");
+			return true;
+		}
+	),
+
+	(logger, shouldExit)
+	{
+		while(!shouldExit())
+		{
+			Thread.sleep( dur!("msecs")( 50 ) );}
+			logger.logInfo("Exiting main function!");
+
+		return 0;
+	}
+);
+
+int main(string[] arguments)
+{
+	version(Windows) string logFilePath = "C:\\logfile.log";
+	else string logFilePath = "logfile.log";
+
+	auto logger = new shared DloggLogger(logFilePath);
+	return buildDaemon!daemon.run(logger);
 }
