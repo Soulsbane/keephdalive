1c45b83ab92f46041d5da3059b12760eb1bb73a1
Paul Crane <paul@acheronsoft.com>
2018-08-18 23:48:34 -0500

    Remove all code related to the daemon.

--- source/keephdalive/app.d
+++ source/keephdalive/app.d
@@ -1,55 +1,7 @@
 import core.time;
 import core.thread;
 
-import daemonize.d;
-
-enum SettingsUpdatedSignal = "SettingsUpdated".customSignal;
-enum LocationsUpdatedSignal = "LocationsUpdated".customSignal;
-
-alias daemon = Daemon!(
-	"keephdalive",
-
-	KeyValueList!(
-		Composition!(Signal.Terminate, Signal.Quit, Signal.Shutdown, Signal.Stop), (logger)
-		{
-			logger.logInfo("Exiting...");
-			return false;
-		},
-		Signal.HangUp, (logger)
-		{
-			logger.logInfo("Hello World!");
-			return true;
-		},
-		SettingsUpdatedSignal, (logger)
-		{
-			logger.logInfo("Updated settings...");
-			return true;
-		},
-		LocationsUpdatedSignal, (logger)
-		{
-			logger.logInfo("Updated locations...");
-			return true;
-		}
-	),
-
-	(logger, shouldExit)
-	{
-		while(!shouldExit())
-		{
-			Thread.sleep( dur!("msecs")( 50 ) );
-		}
-
-		logger.logInfo("Exiting main function!");
-
-		return 0;
-	}
-);
-
 int main(string[] arguments)
 {
-	version(Windows) string logFilePath = "C:\\logfile.log";
-	else string logFilePath = "logfile.log";
-
-	auto logger = new shared DloggLogger(logFilePath);
-	return buildDaemon!daemon.run(logger);
+	return 0;
 }
