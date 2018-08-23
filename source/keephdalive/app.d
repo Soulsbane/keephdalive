d024a1c202e4c15aa899ae93fdd39bfd34b52df2
Paul Crane <paul@acheronsoft.com>
2018-08-23 17:22:34 -0500

    Add overrides from CommandServer.

--- source/keephdalive/app.d
+++ source/keephdalive/app.d
@@ -1,14 +1,41 @@
 import simpleserver;
 
-class KeepAliveApp : SimpleServer
+class KeepAliveApp : CommandServer
 {
-	override void onCommand(const string[] commands)
+	override void onCommand(const string command)
 	{
+		switch(command)
+		{
+			case "restart": break;
+			case "start": break;
+			case "pause": break;
+			case "resume": break;
+			default: break;
+		}
+	}
+
+	override void onCommand(const string command, const string subCommand)
+	{
+	}
+
+	override void onCommand(const string command, const string subCommand, const string value)
+	{
+		if(command == "add" && subCommand == "location")
+		{
+			import std.stdio : writeln;
+			writeln(value);
+			// Add value
+		}
 
 	}
+
+	override void onNoCommands(){}
 }
 
 int main(string[] arguments)
 {
+	auto app = new KeepAliveApp;
+	app.start();
+
 	return 0;
 }
